e287b6baf751b94b3384a1384acb2b69c1ed29ca
Jaydev Thomke <jaydev.thomke@firstcry.com>
2020-03-07 18:36:35 +0530

    Images API

--- api/cereals/controller.js
+++ api/cereals/controller.js
@@ -10,7 +10,6 @@ const cereals = [{
   image: 'masoor'
 }]
 var path = require('path');
-
 const getImage = (req, res) => {
   res.sendFile(path.resolve('assets/' + req.params.img + '.png'));
 };
@@ -26,6 +25,8 @@ const getCereals = (req, res) => {
 const setCereals = (req, res) => {
   if (req) {
     console.log(req.params);
+    cereals[0].quantity = (req.param.tur / 4095) * 100;
+    cereals[1].quantity = (req.param.masoor / 4095) * 100;
     return res.status(200).json(cereals);
   } else {
     return cereals;
