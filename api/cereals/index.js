a39ed1c4cc0a505ba979ccf5fa397bb8ab9ad684
Jaydev Thomke <jaydev.thomke@firstcry.com>
2020-03-10 18:22:29 +0530

    Commit

--- api/cereals/index.js
+++ api/cereals/index.js
@@ -3,7 +3,7 @@ var express = require('express')
 const controller = require('./controller');
 // POST /verication?token=[string]&email=[string]
 router.get('/api/cereals', controller.getCereals);
-router.post('/api/update/cereals/:tur/:masoor', controller.setCereals);
+router.post('/api/update/cereals/:tur', controller.setCereals);
 router.get('/api/image/:img', controller.getImage);
 router.post('/api/update/cereal/name/:id/:name', controller.setCerealName);
 router.post('/api/cereal/add/:name', controller.addCereal);
